569d1d96602922cab0df8777428eeb6be9cdc587
aricept <aricept@users.noreply.github.com>
2017-12-04 18:44:44 -0600

    Merge changes from Master, fixed unhover

--- js/links.js
+++ js/links.js
@@ -3,19 +3,19 @@ var sections = [
 		name: "Video Sales",
 		links: [
 			{
-				title: "SOAR Offer Rate/Quality",
+				desc: "SOAR Offer Rate/Quality",
 				url: "https://workspace.web.att.com/sites/salesexecution/SOAROffer%20Rate/Forms/AllItems.aspx"
 			},
 			{
-				title: "1-3-5-8-12 Video Participation",
+				desc: "1-3-5-8-12 Video Participation",
 				url: "https://workspace.web.att.com/sites/MSR//Sales_and_Service/IP%20Participation/Forms/AllItems.aspx"
 			},
 			{
-				title: "Install Intervals",
+				desc: "Install Intervals",
 				url: "https://workspace.web.att.com/sites/MSR/Sales_and_Service/SS%20DTV%20Install%20Report/Forms/AllItems.aspx"
 			},
 			{
-				title: "Walk Out Watching",
+				desc: "Walk Out Watching",
 				url: "https://workspace.web.att.com/sites/sales_ctr_rpts/Shared%20Documents/Forms/AllItems.aspx?RootFolder=/sites/sales_ctr_rpts/Shared%20Documents/Reports/Cancel%20Rate&FolderCTID=0x0120007C803550EC8BC94E8BE20A776323B77B&View=%7bA90E1132-225F-4877-B1AA-187804860F3C%7d"
 			}
 		]
@@ -25,19 +25,19 @@ var sections = [
 		name: "Video Cancels",
 		links: [
 			{
-				title: "Daily Cancel Rate - Mahdi File",
+				desc: "Daily Cancel Rate - Mahdi File",
 				url: "https://workspace.web.att.com/sites/salesexecution/Daily%20Cancel%20Rate/Forms/AllItems.aspx"
 			},
 			{
-				title: "Daily Cancel Details – R&A File",
+				desc: "Daily Cancel Details – R&A File",
 				url: "https://workspace.web.att.com/sites/MSR//Direct.TV.Details/MCC%20Direct%20TV%20Cancel%20Details/Forms/AllItems.aspx"
 			},
 			{
-				title: "MTD Cancels – Dub File",
+				desc: "MTD Cancels – Dub File",
 				url: "https://workspace.web.att.com/sites/MSR/Sales_and_Service/Cancel%20Reports/Forms/AllItems.aspx?RootFolder=%2Fsites%2FMSR%2FSales%5Fand%5FService%2FCancel%20Reports%2F2017%20SS%20IF%20and%20OOF%20Combined%20Video%20Cancels%20%28April%20forward%29&FolderCTID=0x012000272297D64FB8D8408BCD79A1E33D55A9&View=%7B6AA08F3F%2DAA4B%2D47ED%2DADEB%2D352707BDEF64%7D"
 			},
 			{
-				title: "Weekly Pre/Post Dispatch Cancel Rate",
+				desc: "Weekly Pre/Post Dispatch Cancel Rate",
 				url: "https://workspace.web.att.com/sites/salesexecution/Weekly%20PrePost%20Dispatch%20Cancel%20Rate/Forms/AllItems.aspx"
 			}
 		]
@@ -47,13 +47,13 @@ var sections = [
 		name: "Broadband",
 		links: [
 			{
-				title: "Daily BB Attach Rate – Mahdi File",
+				desc: "Daily BB Attach Rate – Mahdi File",
 				url: "https://workspace.web.att.com/sites/salesexecution/Daily%20Attach%20Rate/Forms/AllItems.aspx"
 			},
 			{
-				title: "1_3_5_8_12 IPBB Participation",
+				desc: "1_3_5_8_12 IPBB Participation",
 				url: "https://workspace.web.att.com/sites/MSR//Sales_and_Service/IP%20Participation/Forms/AllItems.aspx"
-			},
+			}
 		]
 	},
 
@@ -61,7 +61,7 @@ var sections = [
 		name: "DIRECTV Now",
 		links: [
 			{
-				title: "DTV Now",
+				desc: "DTV Now",
 				url: "https://workspace.web.att.com/sites/DTVNOWChannel/DTV%20NOW/Forms/AllItems.aspx"
 			}
 		]
@@ -71,23 +71,23 @@ var sections = [
 		name: "Efficiency",
 		links: [
 			{
-				title: "Genesys",
+				desc: "Genesys",
 				url: "http://att-advisor.it.att.com/nav-service/login.html"
 			},
 			{
-				title: "Utilization",
+				desc: "Utilization",
 				url: "\\GAALPA1CDFILE66.itservices.sbc.com\WFM_CENTERS_REPORTS"
 			},
 			{
-				title: "Intraday Dashboard",
+				desc: "Intraday Dashboard",
 				url: "http://intraday.web.att.com"
 			},
 			{
-				title: "Workforce Ops/CX&O",
+				desc: "Workforce Ops/CX&O",
 				url: "https://workspace.web.att.com/sites/Reporting/SitePages/MCOS%20WFO%20Reporting.aspx"
 			},
 			{
-				title: "SRS/PAR",
+				desc: "SRS/PAR",
 				url: "http://srs.cingular.net"
 			}
 		]
@@ -96,48 +96,48 @@ var sections = [
 		name: "Sales Integrity",
 		links: [
 			{
-				title: "Integrity Operations Home",
+				desc: "Integrity Operations Home",
 				url: "https://workspace.web.att.com/sites/INTEGRITY_OPS/SitePages/Home.aspx"
 			},
 			{
-				title: "U-MART",
+				desc: "U-MART",
 				url: "http://myumart.web.att.com/legal.aspx?viewLegal=yes&retURL=http%3a%2f%2fmyumart.web.att.com%2f"
-			},
+			}
 		]
 	},
 	{
 		name: "Coaching",
 		links: [
 			{
-				title: "Qfiniti",
+				desc: "Qfiniti",
 				url: "http://qfiniti-consumer.bodc.att.com/QWA/Pages/Home.aspx"
 			},
 			{
-				title: "IMC",
+				desc: "IMC",
 				url: "https://www.e-access.att.com/dashboard/imc"
 			},
 			{
-				title: "Leadership Academy Portal",
+				desc: "Leadership Academy Portal",
 				url: "https://intra4.web.att.com/hrtu/LeadershipAcademy/resources_frontline.cfm"
 			},
 			{
-				title: "MyCoach",
+				desc: "MyCoach",
 				url: "http://mycoach.it.att.com/DayBreak/dashboardAction.do?null"
 			},
 			{
-				title: "MyResults",
+				desc: "MyResults",
 				url: "http://salesdashboard.it.att.com/DayBreak/dashboardAction.do"
 			},
 			{
-				title: "Hierarchy Files",
+				desc: "Hierarchy Files",
 				url: "https://tspace.web.att.com/communities/service/html/communityview?communityUuid=dd19cdfd-474c-466f-8cbd-583ed45d70bf"
 			},
 			{
-				title: "Bootcamp Scorecards",
+				desc: "Bootcamp Scorecards",
 				url: "https://workspace.web.att.com/sites/salesexecution/ATT%20Boot%20Camp/Forms/AllItems.aspx"
 			},
 			{
-				title: "Microstrategy",
+				desc: "Microstrategy",
 				url: "http://ccpmbi.web.att.com:8080/MicroStrategy/servlet/mstrWeb"
 			}
 		]
@@ -147,15 +147,15 @@ var sections = [
 		name: "Trouble Tickets",
 		links: [
 			{
-				title: "MOWITS",
+				desc: "MOWITS",
 				url: "http://mytickets.web.att.com/"
 			},
 			{
-				title: "USH Ticket",
+				desc: "USH Ticket",
 				url: "http://ushportal.it.att.com/step1.cfm"
 			},
 			{
-				title: "Qfiniti Ticket",
+				desc: "Qfiniti Ticket",
 				url: "https://workspace.web.att.com/sites/CTST//CQM/Lists/Qfiniti%20Playback%20Issue%20Tracking/AllItems.aspx?View={068370ac-bb2d-4e93-94d4-f6193b0f5cf7}&SortField=Your_x0020_City_x0020_Location&SortDir=Asc"
 			}
 		]
@@ -174,7 +174,7 @@ var sections = [
 /* Link Template
 
 			{
-				title: "",
+				desc: "",
 				url: ""
 			},
 */

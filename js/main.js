72b48d6e622d9bd3b6634f3c1ba4ecf0f891ea5a
aricept <aricept@users.noreply.github.com>
2017-12-05 23:00:34 -0600

    Better animation and IE7 support

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
+function supportsTransitions() {
+	/*credit to the following StackExchange:
+	https://stackoverflow.com/questions/7264899/detect-css-transitions-using-javascript-and-without-modernizr */
+    var b = document.body || document.documentElement,
+        s = b.style,
+        p = 'transition';
+
+    if (typeof s[p] == 'string') { return true; }
+
+    // Tests for vendor specific prop
+    var v = ['Moz', 'webkit', 'Webkit', 'Khtml', 'O', 'ms'];
+    p = p.charAt(0).toUpperCase() + p.substr(1);
+
+    for (var i=0; i<v.length; i++) {
+        if (typeof s[v[i] + p] == 'string') { return true; }
+    }
+
+    return false;
+};
+
 var Section = function(section) {
 	var self = this;
 	self.name = ko.observable(section.name);
@@ -5,6 +25,22 @@ var Section = function(section) {
 	self.img = section.img;
 	self.shown = ko.observable(false);
 	self.slideOut = ko.observable(false);
+	self.slideT = ko.observable(false);
+	self.slideTimer = function(v) {
+		if (v) {
+			self.slideT(false);
+		};
+		if (supportsTransitions()) {
+			var t = setTimeout(function() {
+				self.slideT(v);
+			}, 200);
+		}
+		else {
+			self.slideT(v);
+		}
+
+		
+	};
 	self.delayClose = ko.observable(false);
 	self.slideIn = ko.observable(true);
 };
@@ -18,16 +54,19 @@ var SectionsModel = function(sectionList) {
 	self.hover = function(which) {
 		if (self.curr()){
 			if (which.name() !== self.curr().name()) {
-				self.curr().delayClose(false).shown(false).slideOut(false);
+				self.curr().delayClose(false).shown(false).slideOut(false).slideT(false);
 			};
 		};
 		self.curr(which);
 		self.globalSel(true);
-		self.curr().shown(true).slideOut(true).delayClose(true);
+		self.curr().slideOut(true);
+		self.curr().slideTimer(true)
+		self.curr().shown(true).delayClose(true);
 	};
 	self.unhover = function() {
 		var checkTime = function() {
 			if (!self.curr().delayClose()) {
+				self.curr().slideT(false);
 				self.curr().shown(false).slideOut(false).delayClose(false);
 				self.curr('');
 				self.globalSel(false);
